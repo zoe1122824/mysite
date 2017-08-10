f8cdacca914823c94706135ab6bad4d93a595182
zoe1122824 <zoe1122824@gmail.com>
2017-08-10 03:15:48 -0400

    first commit

---
+++ static/maze teacher/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1502332106,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tilemap.png",
+		"images/player-sheet0.png",
+		"images/skin-sheet0.png",
+		"images/door-sheet0.png",
+		"images/door-sheet1.png",
+		"images/door-sheet2.png",
+		"images/key-sheet0.png",
+		"images/key-sheet1.png",
+		"images/pass-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
---
+++ static/pong/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1502085596,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bara-sheet0.png",
+		"images/ball-sheet0.png",
+		"images/wall-sheet0.png",
+		"images/addscorea-sheet0.png",
+		"images/txtscorea.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
